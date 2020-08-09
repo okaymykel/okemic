76edf0eb1becf385e3643df7b640d2750c2cfbb9
Lanre039 <rajilat1000@gmail.com>
2020-08-09 20:15:38 +0100

    handled the navbar and some other code clean

--- app.js
+++ app.js
@@ -2,30 +2,19 @@
 
 const nav = document.querySelector('#nav');
 const title = document.querySelectorAll('.title');
+const watch_tv = document.querySelector('.watch_tv');
 const effect = document.querySelector('.effect');
 const container = document.querySelector('.container');
 
 // change background on scroll
 document.onwheel = changeBackground
 function changeBackground(event) {
-    // console.log(container.scrollHeight)
-    // console.log('Top', container.scrollTop)
-    // console.log('Client', container.clientHeight)
-    // console.log('scroll to', container.scroll())
-
-    console.log(event.deltaY)
-    if (event.deltaY >= 0   ) {
+   
+    if (event.deltaY > 0) {
         const currentColor = effect.style.backgroundColor;        
         nav.style.background = currentColor  ? currentColor : '#000';  
-        // moon.style.color = currentColor  ? '#000' : '#fff';  
         moon.style.color = nav.style.background == 'rgb(255, 255, 255)' ? '#000' : '#fff';
-        // if (nav.style.background == 'rgb(255, 255, 255)') {
-            // moon.style.color = '#000'
-        // }
-        // if (nav.style.background == 'rgb(0, 0, 0)') {
-        //     moon.style.color = '#fff'
-        // }
-        
+     
         nav.style.color = moon.style.color;
     } else {
         nav.style.background = 'transparent';
@@ -34,14 +23,6 @@ function changeBackground(event) {
         nav.style.color = '#fff';
     }
     
-    // if (nav.style.background == 'rgb(255, 255, 255)') {
-    //     moon.style.color = '#000'
-    //     nav.style.color = '#000';
-    // } else {
-    //     moon.style.color = '#fff'
-    //     nav.style.color = '#fff';
-    
-    // }
 }
 
 const titleArr = Array.from(title);
@@ -54,53 +35,37 @@ function changeTitleSyle(value) {
 const moon = document.querySelector('#moon');
 function handleClickEvent() {
 
-    // if (!nav.style.background) {
-    //     return
-    // }
     nav.style.background = 'transaparent';
     
-    if (nav.style.background && effect.style.backgroundColor == 'rgb(255, 255, 255)') {
-        console.log('dghddggdg')
+    if (effect.style.backgroundColor == 'rgb(255, 255, 255)') {
         effect.style.backgroundColor = '#111418';
-        // if (nav.style.background == 'transparent') {
-            // nav.style.background = '#000';
-            // }
-            // title.style.color = '#fff';
-            changeTitleSyle('#fff');
-        if (!nav.style.background) {
-                return
-            }
+        watch_tv.style.backgroundImage =  "linear-gradient(to right top, rgba(0, 0, 0, 0.1), #000), url('images/ae2f001213e74f45eac9ef1e142f321391587ac2-shutterstock_274681841.jpg')";
+        
+        changeTitleSyle('#fff');
+        
+        if (!nav.style.background) { return }
         nav.style.background = nav.style.background  ? '#000' : nav.style.background;
         moon.style.color = '#fff';
-        // return;
+        
         nav.style.color = '#fff';
     } else {
         effect.style.backgroundColor = '#fff';
+        watch_tv.style.backgroundImage =  "linear-gradient(to right top, rgba(255,30,72, .65), #ED867D), url('images/ae2f001213e74f45eac9ef1e142f321391587ac2-shutterstock_274681841.jpg')";
+        
         changeTitleSyle('#000');
-        // title.style.color = '#000';
-        if (!nav.style.background) {
-            return
-        }
+        
+        if (!nav.style.background) { return }
         nav.style.background = nav.style.background  ? '#fff' : nav.style.background;
         moon.style.color = '#000'
         nav.style.color = '#000';
             
     }
 
-    // }
-    // if (nav.style.background == 'rgb(255, 255, 255)') {
-    //     // nav.style.background = '#111418';
-    //     // if (nav.style.background == 'transparent') {
-    //         nav.style.background = '#000';
-        // }
-        // return;
-    // } 
-    // nav.style.background = effect.style.backgroundColor ==
-    
 }
+
 moon.addEventListener('click', handleClickEvent);
 
-
+// Copyright
 const copyright = document.querySelector('.copyright');
 const date = new Date();
 copyright.innerHTML = `Copyright @ ${date.getFullYear()} nvivo.tv`;
