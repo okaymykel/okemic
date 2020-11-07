9518d49f477210cec7440e36cdf8b24161a1009b
Issa Taofeek <taofeeq.issa076@gmail.com>
2020-11-07 11:23:37 +0100

    taofeek-backend-task1

---
+++ private/db_credentials.php
@@ -0,0 +1,8 @@
+<?php 
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "user");
+define("DB_PASS", "secret");
+define("DB_NAME", "envivo_blog");
+
+?>
---
+++ private/shared/blog_footer.php
@@ -0,0 +1,20 @@
+
+    <footer>
+        &copy; <?php echo date('Y'); ?> Taofeek envivo's blog
+    </footer>
+  </body>
+</html>
+
+<?php 
+  db_disconnect($db);
+?>
+
+
+
+
+
+
+
+
+
+
